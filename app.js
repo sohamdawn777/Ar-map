f45885f3ca252d26ecc3610c5dfa829a5f681ffd
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-01 22:42:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import L from "https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.esm.js";
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.0/build/three.module.js";
-  import { ARButton } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/webxr/ARButton.js";
-  import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/loaders/GLTFLoader.js";
+//import L from "https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.esm.js";
+//import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.0/build/three.module.js";
+  //import { ARButton } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/webxr/ARButton.js";
+ // import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/loaders/GLTFLoader.js";
 
 window.addEventListener("DOMContentLoaded", () => { 
 
