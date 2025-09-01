b150369b8f0c47385879a378103bc64ae759939b
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-01 10:04:27 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.0/build/three.module.js";
+  import { ARButton } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/webxr/ARButton.js";
+  import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/loaders/GLTFLoader.js";
