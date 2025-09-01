52695f502ed231d980ff65d49dbd1ff00eadd92d
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-01 11:20:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,4 +21,5 @@ const camera= new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeig
 scene.add(camera);
 const renderer= new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElemenet);
 
