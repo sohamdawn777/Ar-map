5677bbd7cdd8ff430b415443af897d178fed26b0
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-01 10:16:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.0/build/three.m
   import { ARButton } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/webxr/ARButton.js";
   import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/loaders/GLTFLoader.js";
 
+const map= L.map("map");
+
 const scene= new THREE.scene();
 const camera= new THREE.PerspectiveCamera({ fov: 60, aspect: window.innerWidth/window.innerHeight, near: 0.1, far: 1000 });
 scene.add(camera);
