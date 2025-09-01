e34c26dce357ea52b7e00341f3c60500581456d4
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-01 11:00:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.0/build/three.m
   import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/loaders/GLTFLoader.js";
 
 const map= L.map("map", { center: [22.526911,88.377648], zoom: 19, maxZoom: 19, minZoom: 1 });
+
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {maxZoom: 19, minZoom: 1, tms: false }).addTo(map);
 
+const marker = L.marker([22.526911,88.377648], { 
+    icon: L.icon({ iconUrl: "graffitiIco.png", iconSize: [32,32], iconAnchor: [16,32], popupAnchor: [0,-32] }),
+    title: "Graffiti Spot", draggable: false, riseOnHover: true }).addTo(map);
 
+marker.bindPopup(`<p>This is sample text.</p>`, { maxWidth: 200, minWidth: 50, autoPan: true, closeButton: true, keepInView: true });
 
 const scene= new THREE.scene();
 const camera= new THREE.PerspectiveCamera({ fov: 60, aspect: window.innerWidth/window.innerHeight, near: 0.1, far: 1000 });
