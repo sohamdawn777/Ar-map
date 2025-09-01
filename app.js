6cfd4c1ceafb7252c145d11dfa9773732ad6fdba
sohamdawn777 <sohamdawn12345@gmail.com>
2025-09-01 10:09:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.0/build/three.m
   import { ARButton } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/webxr/ARButton.js";
   import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/loaders/GLTFLoader.js";
 
+const scene=new THREE.scene();
 
